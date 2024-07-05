856fd6210ec6c3adccb033236068e74db7c9a1b8
Ed Balbino <ebalbino93@gmail.com>
2024-07-04 22:27:00 -0300

    Updates public interface.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+pub mod bindings;
 pub mod arena;
 pub mod intern;
 pub mod math;
 
 pub use arena::{Arena, ArenaHandle};
-pub use intern::Intern;
+pub use intern::{StrPool};
 pub use math::{Vec2, Vec3, Vec4, Point};
