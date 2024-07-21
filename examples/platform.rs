ab1a12f61e4050b3cf91f12698dbb68624cd1565
Ed Balbino <ebalbino93@gmail.com>
2024-07-21 19:20:01 -0300

    Changes example.

--- examples/platform.rs
+++ examples/platform.rs
@@ -1,16 +1,22 @@
-use monolith::arena::Arena;
-use monolith::platform::unix::filesystem::Filesystem;
+use monolith::platform::unix::filesystem::{Filesystem, INode};
+use std::ops::Deref;
+
+fn print_entries(root_node: &Vec<INode>) {
+    for entry in root_node {
+        match entry {
+            INode::File(string) => {
+                println!("{}", string.deref());
+            }
+            INode::Directory(nodes) => {
+                print_entries(&nodes);
+            }
+        }
+    }
+}
 
 fn main() {
-    let arena = Arena::new(1024 * 1024 * 16);
-    let filesystem = Filesystem::new();
+    let filesystem = Filesystem::new(".");
+    let root = filesystem.read();
 
-    let file = filesystem.open("./data/shaders/basic.vert");
-    let data = file.read(&arena);
-    let string = core::str::from_utf8(&data).unwrap();
-
-    println!("{} bytes", file.size());
-    println!("{}", string);
-
-    file.append("#define TEST 0\n".as_bytes())
+    print_entries(&root);
 }
